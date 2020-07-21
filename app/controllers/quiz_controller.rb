2491cce4d18535427d5567c57e601e62e9a4b4dd
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-21 16:43:05 -0600

    add functional quiz button to welcome page, create quiz controller, quiz#new action and view

---
+++ app/controllers/quiz_controller.rb
@@ -0,0 +1,4 @@
+class QuizController < ApplicationController
+  def new
+  end
+end
--- spec/features/visitor/welcome_spec.rb
+++ spec/features/visitor/welcome_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 
 describe 'Visitor Welcome Page' do
+  before do
+     visit '/'
+  end
+
   it 'I can see the I Scream Aracade title' do
-    visit '/'
     expect(page).to have_css('.masthead')
+    expect(page).to have_content('I Scream Arcade')
+  end
+
+  it 'I can click button to take Fear Quiz' do
+    within('.fear-quiz') do
+      expect(page).to have_content('Will it Scare Me?')
+      expect(page).to have_content('Take the Fear Quiz')
+      click_on('Will it Scare Me?')
+      expect(current_path).to eq('/quiz')
+      save_and_open_page
+    end
+  end
+
+  it 'I can search by game title or keyword' do
+    within('.game-search') do
+    expect(page).to have_content('Search for Games')
+    end
   end
 end
