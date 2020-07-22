ea92d2066320e71e447fe7da312d64e0fea21806
Ross Perry <perryr16@gmail.com>
2020-07-22 15:49:25 -0600

    front end style functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,20 @@ ghost_array = ["https://images.vexels.com/media/users/3/181081/isolated/lists/bf
               "https://www.clipartmax.com/png/middle/45-459178_drawn-ghost-clipart-transparent-background-ghost-no-background.png",
               "https://i.pinimg.com/originals/46/4b/26/464b2632d12afc02c9a3447c30ded298.jpg"]
 
+spooky_array = [
+  "https://www.vhv.rs/dpng/d/136-1366570_spooky-tree-clip-art-transparent-cartoons-spooky-tree.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHtlcwAA8dbD_v316N4nD6vv167IyPfq8BEw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnqTyM9hT8JvbyteSFFexyFUroTHZCih8HVw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKdoe0H-tzGxvtlQqINfzvte7R1Kf2E4df-w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVfiblxurbZmMchkPfRiWlekK1oHZ_SALgkw&usqp=CAU",
+  "https://banner2.cleanpng.com/20180713/xr/kisspng-smiley-ghost-spirit-halloween-clip-art-bad-spirits-5b4924360558c5.9826495615315200540219.jpg"
+] 
+Game.destroy_all
 30.times do 
     Game.create!(
-      title: Faker::Games::Pokemon.name,
+      title: Faker::Games::SuperSmashBros.fighter,
       agg_rating: rand(1..100),
-      cover: ghost_array.sample,
+      cover: spooky_array.sample,
       popularity: rand(1..100),
       video: "http://Video_URL.com"
     )
