a850398cc3cb78fbc2964f8e3fdea1742106c5dc
Ross Perry <perryr16@gmail.com>
2020-07-23 08:41:11 -0600

    Update profile success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,44 @@ class UsersController < ApplicationController
   end
 
   def update 
-
+    if missing_params.present?
+      missing_params_error
+    else
+      update_profile_success
+    end
   end
 
   private
+
+  def update_params
+    params.require(:user).permit(:email, :name, :photo)
+  end
+
+  def missing_params
+    missing_params = []
+    params[:user].each do |key, value|
+      missing_params << key if value == ''
+    end
+    missing_params.join(', ')
+  end
+
+  def missing_params_error
+    flash[:error] = "Please enter a #{missing_params}."
+    redirect_to edit_user_path(@current_user)
+  end
+
+  def update_profile_success
+    user = User.find(params[:id])
+    user.update(update_params)
+    if user.save
+      flash[:notice] = "You have updated your profile"
+      current_user.reload
+    end
+    redirect_to '/profile'
+  end
+
+
+
 
   def backup_user 
     User.find_by(name: "George Washington")
--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -24,4 +24,59 @@ describe "user show page" do
     expect(current_path).to eq(edit_user_path(@user))
   end
 
+  it "can edit profile when all fields are filled out" do
+    visit edit_user_path(@user)
+
+    photo_url =  "https://cdn.science101.com/wp-content/uploads/2018/08/smart-beagle-810x608-1535480926636.jpg"
+
+    fill_in "user[name]", with: "Dog Bark"
+    fill_in "user[email]", with: "pup@woof.com"
+    fill_in "user[photo]", with: photo_url 
+
+    click_on "Update Profile"
+
+    expect(current_path).to eq('/profile')
+    expect(page).to have_content("You have updated your profile")
+    
+    within(".user-info")do
+      expect(page).to have_content("Dog Bark")
+      expect(page).to have_content("pup@woof.com")
+      expect(page).to_not have_content("name1")
+      expect(page).to_not have_content("email@email.com")
+    end
+
+    within(".user-photo")do
+      expect(page).to have_css("img[src*='#{photo_url}']")
+      expect(page).to_not have_css("img[src*='https://ftnj.com/wp-content/uploads/2018/09/female-headshot-silhouette.jpg']")
+    end
+  end
+
+    it "can edit profile when all fields are filled out" do
+    visit edit_user_path(@user)
+
+    photo_url =  "https://cdn.science101.com/wp-content/uploads/2018/08/smart-beagle-810x608-1535480926636.jpg"
+
+    fill_in "user[name]", with: "Dog Bark"
+    fill_in "user[email]", with: "pup@woof.com"
+    fill_in "user[photo]", with: photo_url 
+
+    click_on "Update Profile"
+
+    expect(current_path).to eq('/profile')
+    expect(page).to have_content("You have updated your profile")
+    
+    within(".user-info")do
+      expect(page).to have_content("Dog Bark")
+      expect(page).to have_content("pup@woof.com")
+      expect(page).to_not have_content("name1")
+      expect(page).to_not have_content("email@email.com")
+    end
+
+    within(".user-photo")do
+      expect(page).to have_css("img[src*='#{photo_url}']")
+      expect(page).to_not have_css("img[src*='https://ftnj.com/wp-content/uploads/2018/09/female-headshot-silhouette.jpg']")
+    end
+  end
+  
+
 end
