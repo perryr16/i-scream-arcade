a45e8c38e2f2cb833fd7b3f6ce8d716cdbfa846b
Ross Perry <perryr16@gmail.com>
2020-07-24 08:32:53 -0600

    remove old migrations

--- db/migrate/20200724141244_drop_videos.rb
+++
@@ -1,5 +0,0 @@
-class DropVideos < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :videos
-  end
-end
--- db/migrate/20200724141451_drop_screenshots.rb
+++
@@ -1,5 +0,0 @@
-class DropScreenshots < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :screenshots
-  end
-end
