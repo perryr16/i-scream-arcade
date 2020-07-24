21df4830a5335caa7f967e11d1b8e7b9392a3948
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-24 11:00:23 -0600

    game_search#show updated to match game#show

--- app/controllers/game_search_controller.rb
+++ app/controllers/game_search_controller.rb
@@ -4,7 +4,6 @@ class GameSearchController < ApplicationController
 
   def index
     @games = Game.all
-    @game = Game.all.first
     #this is just for getting the page up and running
 
     if params[:search_type] == "keyword"
