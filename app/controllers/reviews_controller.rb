80ea20f5dfc82c6465e867240cfb421e2bedd97e
Melanie Tran <melaniexx02@gmail.com>
2020-07-25 21:34:19 -0600

    Add create action in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,17 @@
 class ReviewsController < ApplicationController
   def new
+    @game = Game.find(params[:game_id])
   end
 
   def create
+    game = Game.find(params[:game_id])
+    game.reviews.create(review_params)
+    redirect_to "/games/#{game.id}"
+  end
+
+  private
+
+  def review_params
+    params.permit(:title, :user_rating, :content)
   end
 end
