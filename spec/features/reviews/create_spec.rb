ef7d4c66f1961eb98afe5166e47270d79270ca4e
Melanie Tran <melaniexx02@gmail.com>
2020-07-25 21:33:08 -0600

    Write test when user creates a new review

--- spec/features/reviews/create_spec.rb
+++ spec/features/reviews/create_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 describe "When a user visits a game's show page" do
-  it "User can click on link to fill out review" do
-    game = create(:game)
+  before :each do
+    @game = create(:game)
+  end
 
-    visit "/games/#{game.id}"
+  it "User can click on link to fill out review" do
+    visit "/games/#{@game.id}"
     click_link "Write a Review"
 
-    expect(current_path).to eq("/games/#{game.id}/review")
+    expect(current_path).to eq("/games/#{@game.id}/review")
+  end
+
+  it "User can write a new review for a game" do
+    visit "/games/#{@game.id}/review"
+
+    fill_in :title, with: 'If you like speed and innocent characters'
+    fill_in :user_rating, with: 4
+    fill_in :content, with: 'I have never left a place with so many scratches.'
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/games/#{@game.id}")
   end
 end
