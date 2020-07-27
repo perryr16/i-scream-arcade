d7a443258e0294584e6d6c06141d353f48fdbe9a
Mariana Cid <cidmariana5@gmail.com>
2020-07-27 15:59:35 -0600

    helper method for caching created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def cache_key_for(model_class, label = "")
-    prefix = model_class.to_s.downcase.plurlize
+    prefix = model_class.to_s.downcase
     count = model_class.count
     max_updated = model_class.maximum(:updated_at)
     [prefix, label, count, max_updated].join("_")
