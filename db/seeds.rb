7019e21849cf780acdffdd6e16fb2e6db6780f5e
Melanie Tran <melaniexx02@gmail.com>
2020-07-28 08:56:23 -0600

    Add stylingsheet to game show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 spooky_array = [
   "https://www.vhv.rs/dpng/d/136-1366570_spooky-tree-clip-art-transparent-cartoons-spooky-tree.png",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHtlcwAA8dbD_v316N4nD6vv167IyPfq8BEw&usqp=CAU",
@@ -13,10 +14,10 @@ spooky_array = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKdoe0H-tzGxvtlQqINfzvte7R1Kf2E4df-w&usqp=CAU",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVfiblxurbZmMchkPfRiWlekK1oHZ_SALgkw&usqp=CAU",
   "https://banner2.cleanpng.com/20180713/xr/kisspng-smiley-ghost-spirit-halloween-clip-art-bad-spirits-5b4924360558c5.9826495615315200540219.jpg"
-] 
+]
 Game.destroy_all
 User.destroy_all
-30.times do 
+30.times do
     Game.create!(
       name: Faker::Games::SuperSmashBros.fighter,
       cover: spooky_array.sample,
@@ -31,6 +32,6 @@ george = User.create!(
       email: "george@delaware.com",
       photo: "https://www.americanhistorycentral.com/wp-content/uploads/2016/08/george-washington-portrait-843x1024.jpg"
     )
-15.times do |index|  
+15.times do |index|
   george.games << Game.all[index]
 end
