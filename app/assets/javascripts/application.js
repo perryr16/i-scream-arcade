caf966ce9b66aa0369cf5516a97ec358e9ccdad5
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-28 17:35:24 -0600

    update js so navbar works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
