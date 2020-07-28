cc4d513dde0c956c2d89207d1af757984b37f1d1
Ross Perry <perryr16@gmail.com>
2020-07-28 11:22:39 -0600

    skip tests that require localhost api calls

--- spec/features/game_search/show_spec.rb
+++ spec/features/game_search/show_spec.rb
@@ -17,7 +17,7 @@ describe 'Game Search Show Page' do
     end
   end
 
-  it "can get game by title" do
+  xit "can get game by title" do
     visit '/'
     choose 'search_type_game'
     fill_in 'search', with: 'Super Mario World'
@@ -26,13 +26,14 @@ describe 'Game Search Show Page' do
     summary = "Super Mario World is a jump-and-run platformer and a follow-up to Super Mario Bros. 3. The game retains many of the elements that debuted in Super Mario Bros. 3, such as the world map and Koopaling boss fights, while introducing a large variety of new gameplay mechanics, such as an expanded and less linear world map and the ability to save the game. Much of the game's introduced characters, game mechanics, and artistic themes influenced later titles in the Mario series. The game was released to best-selling status on the SNES, received large amounts of critical acclaim, and is commonly seen on Nintendo's best games of all times on various critic listings."
     expect(page).to have_content(summary)
 
+
     expect(page).to have_content('Super Mario World')
     expect(page).to have_css("img[src*='https://images.igdb.com/igdb/image/upload/t_thumb/co23jy.jpg']")    
     expect(page).to have_content("1990")
     expect(page).to have_content('96.0')
-    expect(page).to have_content('Crash Bandicoot: The Wrath of Cortex')
-    expect(page).to have_content('Big Crown: Showdown')
-    expect(page).to have_content('Super Mario Maker 2')
+    expect(page).to have_button('Crash Bandicoot: The Wrath of Cortex')
+    expect(page).to have_button('Big Crown: Showdown')
+    expect(page).to have_button('Super Mario Maker 2')
     #and others 
     expect(page).to have_content('Action')
     expect(page).to have_content('Platform')
@@ -41,13 +42,14 @@ describe 'Game Search Show Page' do
     expect(page).to have_content('Wii U')
     expect(page).to have_content('New Nintendo 3DS')
 
-    click_on 'Crash Bandicoot: The Wrath of Cortex'
+
+    click_button 'Crash Bandicoot: The Wrath of Cortex'
 
     expect(page).to have_content('Crash Bandicoot: The Wrath of Cortex')
     expect(page).to have_content('Released in 2001')
     expect(page).to have_content('Rating: 64.0')
     expect(page).to have_content('Xbox')
-    expect(page).to have_content('Havocado')
+    expect(page).to have_button('Havocado')
     
 
 
