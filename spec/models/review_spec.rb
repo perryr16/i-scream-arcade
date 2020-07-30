510efd9469755b310bd9a032f8751903c5ff78f2
Melanie Tran <melaniexx02@gmail.com>
2020-07-30 10:30:47 -0600

    Make relationship between user and reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   describe "relationships" do
     it { should belong_to :game }
+    it { should belong_to :user}
   end
 
   describe "validations" do
